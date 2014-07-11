16349001dbaa0864d815579e5e72743c1cffdec0
Eric Wallace <eewallace@gmail.com>
2014-07-11 12:39:33 -0700

    Fix shutdown alias and move it to aliases.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,5 @@ alias chromium='chromium --data-cache-dir=/tmp --data-cache-size=50M'
 alias gmail='chromium --app=https://mail.google.com/mail'
 alias pandora='chromium --app=http://www.pandora.com --user-data-dir=~/.config/chromium/Pandora'
 
+#Systemd shutdown command
+alias shutdown='systemctl poweroff'
--- zshenv
+++ zshenv
@@ -1,4 +1,3 @@
 typeset -U path
 path=(`ruby -rubygems -e "puts Gem.user_dir"`/bin ~/bin $path ~/.rvm/bin .)
 [[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
-alias shutdown='systemctl halt'
